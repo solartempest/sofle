b752c0594cdcf099b6a57d29e25835805b1a0557
solartempest <82990256+solartempest@users.noreply.github.com>
2021-07-30 10:46:34 -0400

    Update in preparation for OLED additions

--- rules.mk
+++ rules.mk
@@ -7,11 +7,13 @@ BOOTLOADER = atmel-dfu
 SPLIT_KEYBOARD = yes	# Split keyboard
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
 CONSOLE_ENABLE = no		# Console for debug
-MOUSEKEY_ENABLE = yes	# Mouse keys(+1482)
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 RGBLIGHT_ENABLE = yes	# RGB lighting
-NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 ENCODER_ENABLE = yes	# Encoders
-OLED_DRIVER_ENABLE = no # Must be disabled if OLEDs are not installed(+3608)
 TAP_DANCE_ENABLE = no	# Tap dance(+1060)
 LTO_ENABLE = yes		# Save space
+
+OLED_DRIVER_ENABLE = no # Must be disabled if OLEDs are not installed(+3608)
+WPM_ENABLE = yes		# For OLED WPM counter
+MOUSEKEY_ENABLE = no	# Mouse keys(+1482)
+NKRO_ENABLE = no		# Has 6KRO+ already. NKRO did not work.
